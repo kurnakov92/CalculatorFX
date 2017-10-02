b64c3f35c9de35fccb34e6ca676ed0b907b92ba4
Oleg <olegkurnakov@mail.ru>
2017-10-02 11:33:47 +0700

    Перевод проекта на maven, подключение JUnit.

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("calculator.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("calculator.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
---
+++ src/test/java/calculator/model/ModelTest.java
@@ -0,0 +1,20 @@
+package calculator.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ModelTest {
+    @Test
+    public void setDividend() throws Exception {
+    }
+
+    @Test
+    public void setDivider() throws Exception {
+    }
+
+    @Test
+    public void division() throws Exception {
+    }
+
+}
